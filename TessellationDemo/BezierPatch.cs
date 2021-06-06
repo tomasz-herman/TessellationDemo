83a54a5790c41499ed9ac09dbd2fe590454c26f9
Tomasz Herman <tomasz.herman@protonmail.com>
2021-06-06 16:05:23 +0200

    Cleanup patch code

--- TessellationDemo/BezierPatch.cs
+++ TessellationDemo/BezierPatch.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 
@@ -8,8 +7,6 @@ namespace TessellationDemo
 {
     public class BezierPatch : IDisposable
     {
-        private Vector3[,] _surface;
-        private List<Vector3> _points;
         public Mesh Patch { get; private set; }
         public Mesh Mesh { get; private set; }
 
@@ -19,83 +16,80 @@ namespace TessellationDemo
             
             BezierPatch patch = new BezierPatch();
 
-            patch._surface = new Vector3[13, 13];
-
+            Vector3[,] surface = new Vector3[13, 13];
+            
             for (int i = 0; i < 13; i++)
             {
                 for (int j = 0; j < 13; j++)
                 {
-                    patch._surface[j, i] = new Vector3(i - 6, heights(j, i), j - 6);
+                     surface[j, i] = new Vector3(i - 6, heights(j, i), j - 6);
                 }
             }
-
-            patch._points = new List<Vector3>();
+            
+            List<int> patchIndices = new List<int>();
 
             for (int i = 0; i < 12; i += 3)
             {
                 for (int j = 0; j < 12; j += 3)
                 {
-                    patch._points.AddRange(
+                    patchIndices.AddRange(
                         new[]
                         {
-                            patch._surface[i, j],
-                            patch._surface[i + 1, j],
-                            patch._surface[i + 2, j],
-                            patch._surface[i + 3, j],
+                            (j + 0) * 13 + (i + 0),
+                            (j + 1) * 13 + (i + 0),
+                            (j + 2) * 13 + (i + 0),
+                            (j + 3) * 13 + (i + 0),
+
+                            (j + 0) * 13 + (i + 1),
+                            (j + 1) * 13 + (i + 1),
+                            (j + 2) * 13 + (i + 1),
+                            (j + 3) * 13 + (i + 1),
                             
-                            patch._surface[i, j + 1],
-                            patch._surface[i + 1, j + 1],
-                            patch._surface[i + 2, j + 1],
-                            patch._surface[i + 3, j + 1],
+                            (j + 0) * 13 + (i + 2),
+                            (j + 1) * 13 + (i + 2),
+                            (j + 2) * 13 + (i + 2),
+                            (j + 3) * 13 + (i + 2),
                             
-                            patch._surface[i, j + 2],
-                            patch._surface[i + 1, j + 2],
-                            patch._surface[i + 2, j + 2],
-                            patch._surface[i + 3, j + 2],
-                            
-                            patch._surface[i, j + 3],
-                            patch._surface[i + 1, j + 3],
-                            patch._surface[i + 2, j + 3],
-                            patch._surface[i + 3, j + 3]
+                            (j + 0) * 13 + (i + 3),
+                            (j + 1) * 13 + (i + 3),
+                            (j + 2) * 13 + (i + 3),
+                            (j + 3) * 13 + (i + 3)
                         }
                     );
                 }
             }
-            
-            List<float> patchPositions = patch._points.SelectMany(vec => new []{ vec.X, vec.Y, vec.Z}).ToList();
-            List<int> patchIndices = Enumerable.Range(0, patch._points.Count).ToList();
-            
-            patch.Patch = new Mesh(
-                patchPositions.ToArray(), 
-                patchIndices.ToArray(), 
-                PrimitiveType.Patches
-            );
 
-            List<float> meshPositions = new List<float>();
+            List<float> positions = new List<float>();
             List<int> meshIndices = new List<int>();
-            for (int i = 0; i < patch._surface.GetLength(0); i++)
-            for (int j = 0; j < patch._surface.GetLength(1); j++)
+            for (int i = 0; i < surface.GetLength(0); i++)
+            for (int j = 0; j < surface.GetLength(1); j++)
             {
-                Vector3 pos = patch._surface[i, j];
-                meshPositions.AddRange(new[] {pos.X, pos.Y, pos.Z});
+                Vector3 pos = surface[i, j];
+                positions.AddRange(new[] {pos.X, pos.Y, pos.Z});
                 
-                int idx = j + i * patch._surface.GetLength(1);
+                int idx = j + i * surface.GetLength(1);
 
-                if (j + 1 < patch._surface.GetLength(1))
+                if (j + 1 < surface.GetLength(1))
                 {
                     meshIndices.AddRange(new[]{idx, idx + 1});
                 }
 
-                if (i + 1 < patch._surface.GetLength(0))
+                if (i + 1 < surface.GetLength(0))
                 {
-                    meshIndices.AddRange(new[]{idx, idx + patch._surface.GetLength(1)});
+                    meshIndices.AddRange(new[]{idx, idx + surface.GetLength(1)});
                 }
             }
             
             patch.Mesh = new Mesh(
-                meshPositions.ToArray(),
+                positions.ToArray(),
                 meshIndices.ToArray(),
                 PrimitiveType.Lines
+            );
+            
+            patch.Patch = new Mesh(
+                positions.ToArray(), 
+                patchIndices.ToArray(), 
+                PrimitiveType.Patches
             );
 
             return patch;
@@ -105,8 +99,6 @@ namespace TessellationDemo
         {
             Patch?.Dispose();
             Mesh?.Dispose();
-            _surface = null;
-            _points.Clear();
             GC.SuppressFinalize(this);
         }
     }
